6c3b9200d4e02ded1fc2df85e02ce24a7050034a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2024-12-16 12:44:36 -0500

    Agregar campo 'codigo' a la entidad TipoEstado y mejorar formato del código

--- src/modules/tipo-estado/entities/tipo-estado.entity.ts
+++ src/modules/tipo-estado/entities/tipo-estado.entity.ts
@@ -1,14 +1,22 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
-    name: 'tipo_estado'
+  name: "tipo_estado",
 })
 export class TipoEstado {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({
-        type: 'varchar'
-    })
-    descripcion: string;
+  @Column({
+    type: "varchar",
+  })
+  descripcion: string;
+
+  @Column({
+    type: "char",
+    length: 1,
+    unique: true,
+    nullable: false
+  })
+  codigo: string;
 }
